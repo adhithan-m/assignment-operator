d7933de47232676d773d3cbc46103246e3739398
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 21:13:06 +0530

    assignment operator.

---
+++ assignment operator/js/assignment.js
@@ -0,0 +1,14 @@
+//assignment operator
+
+var a=parseInt(prompt("enter the a value"));
+var b=parseInt(prompt("enter the b value"));
+//var z = a+=b;
+//alert(z);
+//a-=b;
+//alert(a);
+/*a*=b;
+alert(a);
+a/=b;
+alert(a);
+a%=b;
+alert(a);*/ 
